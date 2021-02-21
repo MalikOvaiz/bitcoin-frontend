166a12d56b5f4f1c4b9ef5d177febe074fdc69d1
Awais <ovaiz.mushtaq@gmail.com>
2021-02-21 02:53:12 +0100

    Integration of Froentend with API

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>Bitcoin - FrontEnd</title>
 
     <!-- Fonts -->
@@ -26,7 +26,7 @@
 
 
 
-<body id="page-top">
+<body id="page-top" onload="setDefaultPriceChart()">
 
     <!-- Navigation -->
 
@@ -43,20 +43,20 @@
             <h2>Bitcoin Price Chart</h2>
             <div class="row">
                 <div class="col">
-                    <p>Start: <input type="text" id="start-datepicker"></p>
+                    <p>Start: <input type="text" id="start_datepicker"></p>
                 </div>
                 <div class="col-6">
-                    <p>End: <input type="text" id="end-datepicker"></p>
+                    <p>End: <input type="text" id="end_datepicker"></p>
                 </div>
                 <div class="col">
-                    <button type="button" class="btn btn-primary">Render</button>
+                    <button type="button" class="btn btn-primary" onclick="setDefaultPriceChart();">Render</button>
                 </div>
             </div>
         </div>
         <div class="container">
 
             <div class="row">
-                <canvas id="myChart" width="400" height="400"></canvas>
+                <canvas id="myChart" width="400" height="200"></canvas>
             </div>
         </div>
     </section>
@@ -73,36 +73,86 @@
     </script>
     <script>
         $(function() {
-            $("#start-datepicker").datepicker();
-            $("#end-datepicker").datepicker({
+            $("#start_datepicker").datepicker({
+                dateFormat: 'dd-mm-yy',
+            });
+            // var date = $('#start_datepicker').datepicker('getDate');
+            // date.setDate(date.getDate() - 10)
+            $("#start_datepicker").datepicker("setDate", "-10");
+
+            $("#end_datepicker").datepicker({
+                dateFormat: 'dd-mm-yy',
                 numberOfMonths: 2,
                 onSelect: function(selected) {
-                    $("#start-datepicker").datepicker("option", "maxDate", selected)
-                }
+                    $("#start_datepicker").datepicker("option", "maxDate", selected)
+                },
             });
+            // var date2 = $('#start-datepicker').datepicker('getDate');
+            // console.log(date2)
+            // $('#end-datepicker').datepicker('setDate', new Date(date2.getDate()));
+            $('#end_datepicker').datepicker("setDate", "today");
         });
     </script>
 
     <script>
-        var ctx = $('#myChart');
-        var myLineChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
-                datasets: [{
-                    data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
-                    label: "Price",
-                    borderColor: "#3e95cd",
-                    fill: false
-                }]
-            },
-            options: {
-                title: {
-                    display: true,
-                    text: 'BitCoin Price Chart'
+        function setDefaultPriceChart() {
+            getBPIAPICall()
+        }
+
+        function intializePriceChart(dates, prices) {
+
+            var ctx = $('#myChart');
+            var myLineChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: dates,
+                    datasets: [{
+                        data: prices,
+                        label: "Price",
+                        borderColor: "#3e95cd",
+                        fill: false
+                    }]
+                },
+                options: {
+                    title: {
+                        display: true,
+                        text: 'BitCoin Price Chart'
+                    }
                 }
-            }
-        });
+            });
+            
+        }
+
+        function getBPIAPICall() {
+
+            var start = $.datepicker.formatDate("yy-mm-dd", $("#start_datepicker").datepicker("getDate"))
+            var end = $.datepicker.formatDate("yy-mm-dd", $("#end_datepicker").datepicker("getDate"))
+
+            let _token = $('meta[name="csrf-token"]').attr('content');
+            $.ajax({
+                type: "GET",
+                url: "http://local.bitcoin-api.com/api/coindesk/historical_bpi_data",
+                datatype: 'json',
+                cache: false,
+                crossDomain: false,
+                data: {
+                    start: start,
+                    end: end,
+                    _token: _token
+                },
+                success: function(response) {
+                    if (response) {
+                        console.log(response.bpi);
+                        dates = Object.keys(response.bpi)
+                        values = Object.values(response.bpi)
+                        intializePriceChart(dates, values)
+                    }
+                },
+                error: function() {
+                    console.log('AJAX load did not work');
+                }
+            });
+        }
     </script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
